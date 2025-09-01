e3af0e2d465d16fd7f0769280ee5599be08ead97
Rafael Araújo <122177980+rafaelaraujo004@users.noreply.github.com>
2025-09-01 11:38:25 -0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-
-const CACHE='pcpgo-v1p2';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
